428089272b767c124dbd796aebd07e80bacd8976
Salvatore Larosa <lrssvtml@gmail.com>
2018-04-03 18:19:47 +0200

    add header to dbmanagement.py

--- dbmanagment.py
+++ dbmanagment.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+        pyArchInit Plugin  - A QGIS plugin to manage archaeological dataset
+                             -------------------
+        begin                : 2007-12-01
+        copyright            : (C) 2008 by Luca Mandolesi
+        email                : mandoluca at gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
 
 from __future__ import absolute_import
 
