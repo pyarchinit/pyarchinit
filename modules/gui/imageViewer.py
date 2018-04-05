0f3ccc4d7bd445e5e169123c8755a805a7de1c93
Salvatore Larosa <lrssvtml@gmail.com>
2018-04-05 23:38:19 +0200

    porting more dialog and modules to py3/pyqt5

--- modules/gui/imageViewer.py
+++ modules/gui/imageViewer.py
@@ -1,7 +1,35 @@
-from imageViewer_ui import Ui_DialogViewer
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+        pyArchInit Plugin  - A QGIS plugin to manage archaeological dataset
+        					 stored in Postgres
+                             -------------------
+    begin                : 2007-12-01
+    copyright            : (C) 2008 by Luca Mandolesi
+    email                : mandoluca at gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+import os
+
+from qgis.PyQt.QtCore import Qt
+from qgis.PyQt.QtWidgets import QDialog, QGraphicsView
+from qgis.PyQt.uic import loadUiType
+
+MAIN_DIALOG_CLASS, _ = loadUiType(os.path.join(os.path.dirname(__file__), 'imageViewer_ui.ui'))
 
 
-class ImageViewer(QDialog, Ui_DialogViewer):
+class ImageViewer(QDialog, MAIN_DIALOG_CLASS):
     def __init__(self, parent=None, origPixmap=None):
         QDialog.__init__(self, parent)
         self.setupUi(self)
