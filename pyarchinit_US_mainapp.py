22832319430f2414138ded72507693b3bd2b218c
Salvatore Larosa <lrssvtml@gmail.com>
2018-04-09 22:36:09 +0200

    fix module import

--- pyarchinit_US_mainapp.py
+++ pyarchinit_US_mainapp.py
@@ -35,14 +35,8 @@ from .modules.utility.pyarchinit_error_check import Error_check
 from .modules.utility.pyarchinit_exp_Periodosheet_pdf import generate_US_pdf
 from .modules.utility.pyarchinit_exp_USsheet_pdf import *
 from .modules.utility.pyarchinit_print_utility import Print_utility
-from  .sortpanelmain import SortPanelMain
-
-# --import pyArchInit modules--#
-try:
-    from .pyarchinit_interactive_matrix_main import pyarchinit_Interactive_Matrix
-    from  pyarchinit_matrix_exp import *
-except:
-    pass
+from .pyarchinit_interactive_matrix_main import pyarchinit_Interactive_Matrix
+from .sortpanelmain import SortPanelMain
 
 MAIN_DIALOG_CLASS, _ = loadUiType(os.path.join(os.path.dirname(__file__), 'modules', 'gui', 'pyarchinit_US_ui.ui'))
 
