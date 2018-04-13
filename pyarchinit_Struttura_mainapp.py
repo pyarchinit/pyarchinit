0fcdad610f060db19143bf71a79baa6b42e01138
Salvatore Larosa <lrssvtml@gmail.com>
2018-04-13 11:58:50 +0200

    Fix #20

--- pyarchinit_Struttura_mainapp.py
+++ pyarchinit_Struttura_mainapp.py
@@ -696,7 +696,7 @@ class pyarchinit_Struttura(QDialog, MAIN_DIALOG_CLASS):
 
     def on_pushButton_delete_pressed(self):
         msg = QMessageBox.warning(self, "Attenzione!!!",
-                                  "Vuoi veramente eliminare il record? \n L'azione è irreversibile", QMessageBox.Cancel,
+                                  "Vuoi veramente eliminare il record? \n L'azione è irreversibile",
                                   QMessageBox.Ok | QMessageBox.Cancel)
         if msg == QMessageBox.Cancel:
             QMessageBox.warning(self, "Messagio!!!", "Azione Annullata!")
