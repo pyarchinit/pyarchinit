1a12e5b349351d37336741aeb5c747e5bfca3334
Salvatore Larosa <lrssvtml@gmail.com>
2018-04-21 09:48:01 +0200

    cleanup: it not longer necessary to precompile ui file them are compiled runtime

--- modules/gui/pyarchinit_Archeozoology.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'pyarchinit_Archeozoology.ui'
-#
-# Created: Tue Oct 29 20:00:52 2013
-#      by: PyQt4 UI code generator 4.9.4
-#
-# WARNING! All changes made in this file will be lost!
