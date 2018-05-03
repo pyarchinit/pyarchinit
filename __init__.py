6a512e08a1b1c93441856bc352c29d3954c22a85
Salvatore Larosa <lrssvtml@gmail.com>
2018-05-03 20:55:09 +0200

    [FEATURE][BONUS] warn to the user if some dependency is missing and allow installing the missing libraries from UI

--- __init__.py
+++ __init__.py
@@ -34,6 +34,36 @@ sys.path.insert(0, os.path.abspath(
 fi = pyarchinit_Folder_installation()
 fi.install_dir()
 
+missing_libraries = []
+try:
+    import pyper
+except Exception as e:
+    missing_libraries.append(str(e))
+
+try:
+    import graphviz
+except Exception as e:
+    missing_libraries.append(str(e))
+
+try:
+    import sqlalchemy
+except Exception as e:
+    missing_libraries.append(str(e))
+
+try:
+    import reportlab
+except Exception as e:
+    missing_libraries.append(str(e))
+
+if missing_libraries:
+    from qgis.PyQt.QtWidgets import QMessageBox
+    res = QMessageBox.warning(None, 'PyArchInit',"Fail importing libraries:\n{}\n\n"
+                                                 "Do you want install the missing libraries?".format(',\n'.join(missing_libraries)), QMessageBox.Ok | QMessageBox.Cancel)
+    if res == QMessageBox.Ok:
+        import subprocess
+        subprocess.call(['python3', '{}'.format(os.path.join(os.path.dirname(__file__), 'scripts', 'modules_installer.py'))])
+    else:
+        pass
 
 def classFactory(iface):
     from .pyarchinitPlugin import PyArchInitPlugin
