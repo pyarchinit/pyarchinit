a647f3cda927e3d9bbc95c9e88e7eb20c9de642e
Salvatore Larosa <lrssvtml@gmail.com>
2018-05-05 15:33:49 +0200

    improvements to installer

--- scripts/modules_installer.py
+++ scripts/modules_installer.py
@@ -22,14 +22,18 @@ import subprocess
 
 import sys
 
+packages = sys.argv[1].split(',') if len(sys.argv) > 2 else []
+print(packages)
+
 # Adding the dependencies python modules in
 # package list in order to install via pip module
-packages = ['PypeR',
-            'SQLAlchemy',
-            'graphviz',
-            'reportlab',
-            'networkx',
-            'matplotlib']
+if not packages:
+    packages = ['PypeR',
+                'SQLAlchemy',
+                'graphviz',
+                'reportlab',
+                'networkx',
+                'matplotlib']
 
 for p in packages:
     if p.startswith('graphviz'):
