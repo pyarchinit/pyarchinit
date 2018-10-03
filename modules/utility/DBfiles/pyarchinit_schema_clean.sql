99d1f4789ebd3ba9e89b46893ac120147c51a945
Salvatore Larosa <lrssvtml@gmail.com>
2018-10-03 12:32:07 +0200

    update pg schema file

--- modules/utility/DBfiles/pyarchinit_schema_clean.sql
+++ modules/utility/DBfiles/pyarchinit_schema_clean.sql
@@ -9,13 +9,13 @@
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
+--SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', 'public', false);
 SET check_function_bodies = false;
 SET client_min_messages = warning;
-SET row_security = off;
+--SET row_security = off;
 
 -- create postgis extension
 CREATE EXTENSION postgis;
