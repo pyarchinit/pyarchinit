90aba4cc3f44b9a2a41bb647f70f563b04e425b2
enzo <enzo.ccc@gmail.com>
2018-12-09 08:28:54 +0100

    add sql file to update db
    aggiunta dei file sql per implementare i nuovi layers eview in postgres e sqlite

---
+++ resources/dbfiles/2.sql
@@ -0,0 +1,9 @@
+
+
+CREATE TABLE "pyarchinit_documentazione" (
+"pkuid" integer PRIMARY KEY AUTOINCREMENT,
+"sito" text,
+"nome_doc" text,
+"tipo_doc" text,
+"path_qgis_pj" text, "the_geom" LINESTRING);
+
