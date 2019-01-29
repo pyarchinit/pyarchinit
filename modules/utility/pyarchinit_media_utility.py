ed673552214985513647a2cccb2098aebc96399c
enzo <enzo.ccc@gmail.com>
2019-01-29 08:30:51 +0100

    Add files via upload

--- modules/utility/pyarchinit_media_utility.py
+++ modules/utility/pyarchinit_media_utility.py
@@ -48,13 +48,13 @@ class Media_utility(object):
         self.outpath = o
         self.thumb_suffix = ts
 
-        size = 2008, 1417
+        size = 150, 150
         infile = str(self.input_path)
         outfile = ('%s%s_%s%s') % (
         self.outpath, str(self.max_num_id), os.path.splitext(self.infile)[0], self.thumb_suffix)
         im = Image.open(infile)
         im.thumbnail(size, Image.ANTIALIAS)
-        im.save(outfile, dpi=(300, 300))
+        im.save(outfile, dpi=(100, 100))
 
 
 if __name__ == '__main__':
@@ -65,18 +65,53 @@ if __name__ == '__main__':
     print(thumb_path_str)
     m.resample_images('/Users/pyarchinit/desktop/Archivio/', 'Immagine2.png', thumb_path_str, '_pay.png')
 
+class Media_utility_resize(object):
+    """
+    >> import Image                      # importo il modulo
+>>> image_path = r"C:\test\snake.png" # assegno l' immagine alla variabile tramite raw string (r"path\files")
+>>> image = Image.open(image_path)    # apro l'immagine
+>>> width, height = (150, 110)        # ridimensiono l'immagine l' originale era 300x350px
+>>> size = (width, height)            # assegno grandezza e altezza
+>>> new_image_thumbnail = r"C:\test\snake_small.png" # che sia pitone grande o  pitone piccolo l' importante che pitone è
+>>> image.thumbnail(size, Image.ANTIALIAS) # creo la thumbnail vera e propria con antialias ma potevamo anche bicubic bilinear ecc
+>>> image.save(new_image_thumbnail)        # salvo la nuova immagine "thumbnail"
+"""
+
+    def resample_images(self, mid, ip, i, o, ts):
+        self.max_num_id = mid
+        self.input_path = ip
+        self.infile = i
+        self.outpath = o
+        self.thumb_suffix = ts
+
+        size = 2008, 1417
+        infile = str(self.input_path)
+        outfile = ('%s%s_%s%s') % (
+        self.outpath, str(self.max_num_id), os.path.splitext(self.infile)[0], self.thumb_suffix)
+        im = Image.open(infile)
+        im.thumbnail(size, Image.ANTIALIAS)
+        im.save(outfile, dpi=(300, 300))
+
+
+if __name__ == '__main__':
+    m = Media_utility_resize()
+    conn = Connection()
+    thumb_resize = conn.thumb_resize()
+    thumb_resize_str = thumb_resize['thumb_resize']
+    print(thumb_resize_str)
+    m.resample_images('/Users/pyarchinit/desktop/Archivio/', 'Immagine2.png', thumb_resize_str, '_pay.png')
 """
 listfiles = os.listdir(self.pathfiles)
 for infile in listfiles:
-	comp_file = ('%s/%s') % (self.pathfiles,infile)
-	outfile = os.path.splitext(infile)[0] + "_pay.png"
-	if infile != outfile: 
-		try:
-			im = Image.open(self.pathfiles + infile)
-			print 'im', im
-			im.thumbnail(size) 
-			im.save(self.outputpath + outfile, dpi=(100,100)) 
+    comp_file = ('%s/%s') % (self.pathfiles,infile)
+    outfile = os.path.splitext(infile)[0] + "_pay.png"
+    if infile != outfile: 
+        try:
+            im = Image.open(self.pathfiles + infile)
+            print 'im', im
+            im.thumbnail(size) 
+            im.save(self.outputpath + outfile, dpi=(100,100)) 
 
-		except IOError: 
-			print "cannot create thumbnail for", infile
+        except IOError: 
+            print "cannot create thumbnail for", infile
 """
