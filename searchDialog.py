e96ac3e5774d896f16ea69d0ee00148a433ab4fb
enzo <enzo.ccc@gmail.com>
2020-04-23 09:54:41 +0200

    add search layer

---
+++ searchDialog.py
@@ -0,0 +1,218 @@
+import os
+import re
+
+from qgis.PyQt.uic import loadUiType
+from qgis.PyQt.QtWidgets import QDialog, QAbstractItemView, QTableWidgetItem
+from qgis.PyQt.QtCore import QThread
+
+from qgis.core import QgsVectorLayer, Qgis, QgsProject, QgsMapLayer
+from .searchWorker import Worker
+
+
+FORM_CLASS, _ = loadUiType(os.path.join(
+    os.path.dirname(__file__), 'searchlayers.ui'))
+
+
+class LayerSearchDialog(QDialog, FORM_CLASS):
+    def __init__(self, iface, parent):
+        '''Initialize the LayerSearch dialog box'''
+        super(LayerSearchDialog, self).__init__(parent)
+        self.setupUi(self)
+        self.iface = iface
+        self.canvas = iface.mapCanvas()
+        # Notify us when vector items ared added and removed in QGIS
+        QgsProject.instance().layersAdded.connect(self.updateLayers)
+        QgsProject.instance().layersRemoved.connect(self.updateLayers)
+        
+        self.doneButton.clicked.connect(self.closeDialog)
+        self.stopButton.clicked.connect(self.killWorker)
+        self.searchButton.clicked.connect(self.runSearch)
+        self.clearButton.clicked.connect(self.clearResults)
+        self.layerListComboBox.activated.connect(self.layerSelected)
+        self.searchFieldComboBox.addItems(['<All Fields>'])
+        self.maxResults = 1500
+        self.resultsTable.setColumnCount(4)
+        self.resultsTable.setSortingEnabled(False)
+        self.resultsTable.setHorizontalHeaderLabels(['Valore','Tabella','Campo','Feature Id'])
+        self.resultsTable.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.comparisonComboBox.addItems(['=','contiene','inizia con'])
+        self.resultsTable.itemSelectionChanged.connect(self.select_feature)
+        self.worker = None
+
+    def closeDialog(self):
+        '''Close the dialog box when the Close button is pushed'''
+        self.hide()
+    
+    def updateLayers(self):
+        '''Called when a layer has been added or deleted in QGIS.
+        It forces the dialog to reload.'''
+        # Stop any existing search
+        self.killWorker()
+        if self.isVisible():
+            self.populateLayerListComboBox()
+            self.clearResults()
+        
+    def select_feature(self):
+        '''A feature has been selected from the list so we need to select
+        and zoom to it'''
+        if self.noSelection:
+            # We do not want this event while data is being changed
+            return
+        # Deselect all selections
+        layers = QgsProject.instance().mapLayers().values()
+        for layer in layers:
+            if layer.type() == QgsMapLayer.VectorLayer:
+                layer.removeSelection()
+        # Find the layer that was selected and select the feature in the layer
+        selectedRow = self.resultsTable.currentRow()
+        selectedLayer = self.results[selectedRow][0]
+        selectedFeature = self.results[selectedRow][1]
+        selectedLayer.select(selectedFeature.id())
+        # Zoom to the selected feature
+        self.canvas.zoomToSelected(selectedLayer)
+    
+    def layerSelected(self):
+        '''The user has made a selection so we need to initialize other
+        parts of the dialog box'''
+        self.initFieldList()
+        
+    def showEvent(self, event):
+        '''The dialog is being shown. We need to initialize it.'''
+        super(LayerSearchDialog, self).showEvent(event)
+        self.populateLayerListComboBox()
+        
+    def populateLayerListComboBox(self):
+        '''Find all the vector layers and add them to the layer list
+        that the user can select. In addition the user can search on all
+        layers or all selected layers.'''
+        layerlist = ['<Tutte le tabelle>','<Seleziona tabella>']
+        self.searchLayers = [None, None] # This is same size as layerlist
+        layers = QgsProject.instance().mapLayers().values()
+        
+        for layer in layers:
+            if layer.type() == QgsMapLayer.VectorLayer:
+                layerlist.append(layer.name())
+                self.searchLayers.append(layer)
+
+        self.layerListComboBox.clear()
+        self.layerListComboBox.addItems(layerlist)
+        self.initFieldList()
+        
+    def initFieldList(self):
+        selectedLayer = self.layerListComboBox.currentIndex()
+        self.searchFieldComboBox.clear()
+        self.searchFieldComboBox.addItem('<Tutti i campi>')
+        if selectedLayer > 1:
+            self.searchFieldComboBox.setEnabled(True)
+            for field in self.searchLayers[selectedLayer].fields():
+                self.searchFieldComboBox.addItem(field.name())
+        else:
+            self.searchFieldComboBox.setCurrentIndex(0)
+            self.searchFieldComboBox.setEnabled(False)
+    
+    def runSearch(self):
+        '''Called when the user pushes the Search button'''
+        selectedLayer = self.layerListComboBox.currentIndex()
+        comparisonMode = self.comparisonComboBox.currentIndex()
+        self.noSelection = True
+        try:
+            sstr = self.findStringEdit.text().strip()
+        except:
+            self.showErrorMessage('Ricerca invalida ')
+            return
+            
+        if str == '':
+            self.showErrorMessage('La stringa cercata è vuota')
+            return
+        if selectedLayer == 0:
+            # Include all vector layers
+            layers = QgsProject.instance().mapLayers().values()
+        elif selectedLayer == 1:
+            # Include all selected vector layers
+            layers = self.iface.layerTreeView().selectedLayers()
+        else:
+            # Only search on the selected vector layer
+            layers = [self.searchLayers[selectedLayer]]
+        self.vlayers=[]
+        # Find the vector layers that are to be searched
+        for layer in layers:
+            if isinstance(layer, QgsVectorLayer):
+                self.vlayers.append(layer)
+        if len(self.vlayers) == 0:
+            self.showErrorMessage('There are no vector layers to search through')
+            return
+        
+        # vlayers contains the layers that we will search in
+        self.searchButton.setEnabled(False)
+        self.stopButton.setEnabled(True)
+        self.doneButton.setEnabled(False)
+        self.clearButton.setEnabled(False)
+        self.clearResults()
+        self.resultsLabel.setText('')
+        infield = self.searchFieldComboBox.currentIndex() >= 1
+        if infield is True:
+            selectedField = self.searchFieldComboBox.currentText()
+        else:
+            selectedField = None
+        
+        # Because this could take a lot of time, set up a separate thread
+        # for a worker function to do the searching.
+        thread = QThread()
+        worker = Worker(self.vlayers, infield, sstr, comparisonMode, selectedField, self.maxResults)
+        worker.moveToThread(thread)
+        thread.started.connect(worker.run)
+        worker.finished.connect(self.workerFinished)
+        worker.foundmatch.connect(self.addFoundItem)
+        worker.error.connect(self.workerError)
+        self.thread = thread
+        self.worker = worker
+        self.noSelection = False
+        thread.start()
+
+    def workerFinished(self, status):
+        '''Clean up the worker and thread'''
+        self.worker.deleteLater()
+        self.thread.quit()
+        self.thread.wait()
+        self.thread.deleteLater()
+        self.worker = None
+        self.resultsLabel.setText('Risultati: '+str(self.found))
+
+        self.vlayers = []
+        self.searchButton.setEnabled(True)
+        self.clearButton.setEnabled(True)
+        self.stopButton.setEnabled(False)
+        self.doneButton.setEnabled(True)
+    
+    def workerError(self, exception_string):
+        '''An error occurred so display it.'''
+        #self.showErrorMessage(exception_string)
+        print(exception_string)
+    
+    def killWorker(self):
+        '''This is initiated when the user presses the Stop button
+        and will stop the search process'''
+        if self.worker is not None:
+            self.worker.kill()
+        
+    def clearResults(self):
+        '''Clear all the search results.'''
+        self.noSelection = True
+        self.found = 0
+        self.results = []
+        self.resultsTable.setRowCount(0)        
+        self.noSelection = False
+    
+    def addFoundItem(self, layer, feature, attrname, value):
+        '''We found an item so add it to the found list.'''
+        self.resultsTable.insertRow(self.found)
+        self.results.append([layer, feature])
+        self.resultsTable.setItem(self.found, 0, QTableWidgetItem(value))
+        self.resultsTable.setItem(self.found, 1, QTableWidgetItem(layer.name()))
+        self.resultsTable.setItem(self.found, 2, QTableWidgetItem(attrname))
+        self.resultsTable.setItem(self.found, 3, QTableWidgetItem(str(feature.id())))
+        self.found += 1        
+            
+    def showErrorMessage(self, message):
+        '''Display an error message.'''
+        self.iface.messageBar().pushMessage("", message, level=Qgis.Warning, duration=2)
---
+++ searchWorker.py
@@ -0,0 +1,136 @@
+import os
+import re
+
+from qgis.PyQt.QtCore import QObject, pyqtSignal
+
+from qgis.core import QgsVectorLayer, QgsFeatureRequest
+import traceback
+
+class Worker(QObject):
+    '''This does all the hard work. It takes all the search parameters and 
+    searches through the vector layers for a match.'''
+    finished = pyqtSignal(bool)
+    error = pyqtSignal(str)
+    foundmatch = pyqtSignal(QgsVectorLayer, object, object, str)
+    
+    def __init__(self, vlayers, infield, searchStr, comparisonMode, selectedField, maxResults):
+        QObject.__init__(self)
+        self.vlayers = vlayers
+        self.infield = infield
+        self.searchStr = searchStr
+        self.comparisonMode = comparisonMode
+        self.selectedField = selectedField
+        self.killed = False
+        self.maxResults = maxResults
+        
+    def run(self):
+        '''Worker Run routine'''
+        self.found = 0
+        try:
+            # Check to see if we are searching within a particular column of a specified
+            # layer or whether we are searching all columns.
+            if self.infield is True:
+                for layer in self.vlayers:
+                    self.searchFieldInLayer(layer, self.searchStr, self.comparisonMode, self.selectedField)
+            else:
+                for layer in self.vlayers:
+                    self.searchLayer(layer, self.searchStr, self.comparisonMode)
+        except:
+            self.error.emit(traceback.format_exc())
+        self.finished.emit(True)
+            
+    def kill(self):
+        '''Set a flag that we want to stop looking for matches.'''
+        self.killed = True
+        
+    def searchLayer(self, layer, searchStr, comparisonMode):
+        '''Do a string search across all columns in a table'''
+        if self.killed:
+            return
+        fnames = []
+        # Get and Keep a copy of the field names
+        for field in layer.fields():
+            fnames.append(field.name())
+        # Get an iterator for all the features in the vector
+        iter = layer.getFeatures(QgsFeatureRequest().setFlags(QgsFeatureRequest.NoGeometry))
+        if comparisonMode == 0: # Searching for an exact match
+            for feature in iter:
+                # Check to see if it has been aborted
+                if self.killed is True:
+                    return
+                # Get all the feature attributes or columns
+                attrs = feature.attributes()
+                # For now just search as if it were a string
+                for id, f in enumerate(attrs):
+                    try:
+                        if str(f) == searchStr:
+                            self.foundmatch.emit(layer, feature, fnames[id], str(f))
+                            self.found += 1
+                            if self.found >= self.maxResults:
+                                self.killed=True
+                                return
+                    except:
+                        pass
+        elif comparisonMode == 1: # contains string
+            p = re.compile(re.escape(searchStr), re.I|re.UNICODE)
+            for feature in iter:
+                # Check to see if it has been aborted
+                if self.killed is True:
+                    return
+                attrs = feature.attributes()
+                # For now just search as if it were a string
+                for id, f in enumerate(attrs):
+                    try:
+                        if p.search(str(f)):
+                            self.foundmatch.emit(layer, feature, fnames[id], str(f))
+                            self.found += 1
+                            if self.found >= self.maxResults:
+                                self.killed=True
+                                return
+                    except:
+                        pass
+        else: # begins with string
+            p = re.compile(re.escape(searchStr), re.I|re.UNICODE)
+            for feature in iter:
+                # Check to see if it has been aborted
+                if self.killed is True:
+                    return
+                attrs = feature.attributes()
+                # For now just search as if it were a string
+                for id, f in enumerate(attrs):
+                    try:
+                        if p.match(str(f)):
+                            self.foundmatch.emit(layer, feature, fnames[id], str(f))
+                            self.found += 1
+                            if self.found >= self.maxResults:
+                                self.killed=True
+                                return
+                    except:
+                        pass
+
+    def searchFieldInLayer(self, layer, searchStr, comparisonMode, selectedField):
+        '''Do a string search on a specific column in the table.'''
+        if self.killed:
+            return
+
+        request = QgsFeatureRequest().setFlags(QgsFeatureRequest.NoGeometry)
+        request.setSubsetOfAttributes([selectedField], layer.fields())
+        if comparisonMode == 0: # Searching for an exact match
+            request.setFilterExpression('"{}" LIKE \'{}\''.format(selectedField,searchStr))
+        elif comparisonMode == 1: # contains string
+            
+            request.setFilterExpression('"{}" ILIKE \'{}%\'+', '+  ILIKE \'{}%\''.format(selectedField,searchStr) )
+        else: # begins with string
+            request.setFilterExpression('"{}" ILIKE \'%{}%\''.format(selectedField,searchStr))
+
+        for feature in layer.getFeatures(request):
+            # Check to see if it has been aborted
+            if self.killed is True:
+                return
+            f = feature.attribute(selectedField)
+            self.foundmatch.emit(layer, feature, selectedField, str(f))
+            self.found += 1
+            if self.found >= self.maxResults:
+                self.killed=True
+                return
+
