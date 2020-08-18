668c688d4bff5bcc6c6e318e98bbb94464a9a6a6
enzo <enzo.ccc@gmail.com>
2020-08-18 16:19:58 +0200

    Add files via upload

--- tabs/Image_viewer.py
+++ tabs/Image_viewer.py
@@ -27,7 +27,7 @@ from builtins import str
 import PIL as Image
 from PIL import *
 import shutil
-#import ffmpeg
+import ffmpeg
 from qgis import PyQt
 from qgis.PyQt.QtCore import *
 from qgis.PyQt.QtGui import *
@@ -470,9 +470,76 @@ class Main(QDialog,MAIN_DIALOG_CLASS):
                 # if video.endswith(".mp4"): #or .avi, .mpeg, whatever.
                     # filenamev, filetypev = video.split(".")[0], video.split(".")[1]  # db definisce nome immagine originale
                     # filepathv = directory + '/' + filenamev + "." + filetypev  # db definisce il path immagine originale
+                    # idunique_video_check = self.db_search_check(self.MAPPER_TABLE_CLASS, 'filepath',
+                                                                # filepathv)
                     # outputfile='{}.png'.format(directory + '/' + filenamev)
                     # infile=ffmpeg.input(filepathv,ss='00:00:4').output(outputfile,vframes=1).overwrite_output()
                     # ffmpeg.run(infile)                                            
+            
+                # if not bool(idunique_video_check):
+                    # mediatype = 'video'  # db definisce il tipo di immagine originale
+                    # self.insert_record_media(mediatype, filenamev, filetypev,
+                                             # filepathv)  # db inserisce i dati nella tabella media originali
+                    # MU = Media_utility()
+                    # MUR = Media_utility_resize()
+                    # media_max_num_id = self.DB_MANAGER.max_num_id(self.MAPPER_TABLE_CLASS,
+                                                                  # self.ID_TABLE)  # db recupera il valore più alto ovvero l'ultimo immesso per l'immagine originale
+                    # thumb_path = conn.thumb_path()
+                    # thumb_path_str = thumb_path['thumb_path']
+                    # thumb_resize = conn.thumb_resize()
+                    # thumb_resize_str = thumb_resize['thumb_resize']
+                    # media_thumb_suffix = '_thumb.mp4'
+                    # media_resize_suffix = '.mp4'
+                    # filenameorig = filenamev
+                    # filename_thumb = str(media_max_num_id) + "_" + filenamev + media_thumb_suffix
+                    # filename_resize = str(media_max_num_id) + "_" +filenamev + media_resize_suffix
+                    # filepath_thumb =  filename_thumb
+                    # filepath_resize = filename_resize
+                    # self.SORT_ITEMS_CONVERTED = []
+                    # # crea la thumbnail
+                    # try:
+                        # MU.resample_images(media_max_num_id, filepathv, filenameorig, thumb_path_str, media_thumb_suffix)
+                        # MUR.resample_images(media_max_num_id, filepathv, filenameorig, thumb_resize_str, media_resize_suffix)
+                    # except Exception as e:
+                        # QMessageBox.warning(self, "Cucu", str(e), QMessageBox.Ok)
+                        # # progressBAr
+                    # try:
+                        # for i in enumerate(image):
+                            # image_list.append(i[0])
+                        # for n in range(len(image_list)):
+                            # self.progressBar.setValue(((n)/100)*100)
+                            # QApplication.processEvents()
+                    # except:
+                        # pass
+                    # self.insert_record_mediathumb(media_max_num_id, mediatype, filenamev, filename_thumb, filetypev,
+                                                  # filepath_thumb, filepath_resize)
+                    # item = QListWidgetItem(str(filenameorig))
+                    # item.setData(Qt.UserRole, str(media_max_num_id))
+                    # icon = QIcon(str(thumb_path_str)+filepath_thumb)
+                    # item.setIcon(icon)
+                    # self.iconListWidget.addItem(item)
+                    # self.progressBar.reset()
+                # elif bool(idunique_video_check):
+                    # data = idunique_video_check
+                    # id_media = data[0].id_media
+                    # media_filename =data[0].filename
+                    # # visualizza le immagini nella ui
+                    # item = QListWidgetItem(str(media_filename))
+                    # data_for_thumb = self.db_search_check(self.MAPPER_TABLE_CLASS_thumb, 'media_filename',
+                                                          # media_filename)  # recupera i valori della thumb in base al valore id_media del file originale
+                    # try:
+                        # thumb_path = data_for_thumb[0].filepath_thumb
+                        # item.setData(Qt.UserRole, thumb_path)
+                        # icon = QIcon(str(thumb_path_str)+filepath_thumb)  # os.path.join('%s/%s' % (directory.toUtf8(), image)))
+                        # item.setIcon(icon)
+                        # self.iconListWidget.addItem(item)
+                    # except:
+                        # pass
+            # if bool(idunique_video_check):
+                # QMessageBox.information(self, "Message", "Le immagini sono già caricate nel database")
+            # elif not bool(idunique_video_check):
+                # QMessageBox.information(self, "Message", "Imamagini caricate! Puoi taggarle")
+            
             for image in sorted(os.listdir(directory)):    
                 if image.endswith(".png") or image.endswith(".PNG") or image.endswith(".JPG") or image.endswith(
                         ".jpg") or image.endswith(".jpeg") or image.endswith(".JPEG") or image.endswith(
@@ -570,7 +637,7 @@ class Main(QDialog,MAIN_DIALOG_CLASS):
                     msg = self.filename + ": Image already in the database"
                 else:
                     msg = e
-                QMessageBox.warning(self, "Errore", "Warning 1 ! \n"+ str(msg),  QMessageBox.Ok)
+                #QMessageBox.warning(self, "Errore", "Warning 1 ! \n"+ str(msg),  QMessageBox.Ok)
                 return 0
         except Exception as  e:
             QMessageBox.warning(self, "Error", "Warning 2 ! \n"+str(e),  QMessageBox.Ok)
@@ -602,7 +669,7 @@ class Main(QDialog,MAIN_DIALOG_CLASS):
                     msg = self.filename + ": thumb already present into the database"
                 else:
                     msg = e
-                QMessageBox.warning(self, "Error", "warming 1 ! \n"+ str(msg),  QMessageBox.Ok)
+                #QMessageBox.warning(self, "Error", "warming 1 ! \n"+ str(msg),  QMessageBox.Ok)
                 return 0
         except Exception as  e:
             QMessageBox.warning(self, "Error", "Warning 2 ! \n"+str(e),  QMessageBox.Ok)
