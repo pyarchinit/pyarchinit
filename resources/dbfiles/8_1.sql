23dc728a7343f52f5d619a73ada3bc605e00e03e
enzococca <enzo.ccc@gmail.com>
2020-10-24 12:28:37 +0200

    -add logo

--- resources/dbfiles/8_1.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE media_thumb_table (id_media_thumb INTEGER NOT NULL, id_media INTEGER, mediatype TEXT, media_filename TEXT, media_thumb_filename TEXT, filetype VARCHAR(10), filepath TEXT, path_resize TEXT, PRIMARY KEY (id_media_thumb), CONSTRAINT "ID_media_thumb_unico" UNIQUE (media_thumb_filename) )
