1b8a35cc273faacdd4277207bba4100f04e9d4fd
enzococca <enzo.ccc@gmail.com>
2020-10-24 15:23:42 +0200

    -update ui inventario materiali

--- tabs/Inv_Materiali.py
+++ tabs/Inv_Materiali.py
@@ -23,6 +23,8 @@ from __future__ import absolute_import
 
 import os
 from datetime import date
+import platform
+from pdf2docx import parse
 import cv2
 import numpy as np
 import sys
@@ -377,6 +379,7 @@ class pyarchinit_Inventario_reperti(QDialog, MAIN_DIALOG_CLASS):
         self.iface = iface
         self.setupUi(self)
         self.mDockWidget_export.setHidden(True)
+        self.mDockWidget.setHidden(True)
         self.pyQGIS = Pyarchinit_pyqgis(iface)
         self.currentLayerId = None
         
@@ -3384,6 +3387,59 @@ class pyarchinit_Inventario_reperti(QDialog, MAIN_DIALOG_CLASS):
         f = open(str(name_file), 'w')
         f.write(str(message))
         f.close()
+
+    def on_pushButton_open_dir_pressed(self):
+        HOME = os.environ['PYARCHINIT_HOME']
+        path = '{}{}{}'.format(HOME, os.sep, "pyarchinit_PDF_folder")
+
+        if platform.system() == "Windows":
+            os.startfile(path)
+        elif platform.system() == "Darwin":
+            subprocess.Popen(["open", path])
+        else:
+            subprocess.Popen(["xdg-open", path])
+    def setPathpdf(self):
+        s = QgsSettings()
+        dbpath = QFileDialog.getOpenFileName(
+            self,
+            "Set file name",
+            self.PDFFOLDER,
+            " PDF (*.pdf)"
+        )[0]
+        #filename=dbpath.split("/")[-1]
+        if dbpath:
+             
+            self.lineEdit_pdf_path.setText(dbpath)
+            s.setValue('',dbpath)
+   
+    def on_pushButton_convert_pressed(self):
+        # if not bool(self.setPathpdf()):    
+            # QMessageBox.warning(self, "INFO", "devi scegliere un file pdf",
+                                # QMessageBox.Ok)
+        
+        try:
+            pdf_file = self.lineEdit_pdf_path.text()
+            filename=pdf_file.split("/")[-1]
+            docx_file = self.PDFFOLDER+'/'+filename+'.docx'
+
+            # convert pdf to docx
+            parse(pdf_file, docx_file, start=self.lineEdit_pag1.text(), end=self.lineEdit_pag2.text())
+            QMessageBox.information(self, "INFO", "Conversione terminata",
+                                QMessageBox.Ok)
+                                
+        except Exception as e:
+            QMessageBox.warning(self, "Error", str(e),
+                                QMessageBox.Ok)
+    def openpdfDir(self):
+        HOME = os.environ['PYARCHINIT_HOME']
+        path = '{}{}{}'.format(HOME, os.sep, "pyarchinit_PDF_folder")
+
+        if platform.system() == "Windows":
+            os.startfile(path)
+        elif platform.system() == "Darwin":
+            subprocess.Popen(["open", path])
+        else:
+            subprocess.Popen(["xdg-open", path])
 
 
 ## Class end
