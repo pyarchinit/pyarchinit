aa6770881357cc509f60748f82b91021ad9c236b
enzococca <enzo.ccc@gmail.com>
2020-10-29 23:06:42 +0100

    -fix bug

--- tabs/US_USM.py
+++ tabs/US_USM.py
@@ -807,7 +807,7 @@ class pyarchinit_US(QDialog, MAIN_DIALOG_CLASS):
         self.toolButton_pdfpath.clicked.connect(self.setPathpdf)
         self.pbnOpenpdfDirectory.clicked.connect(self.openpdfDir)
         self.progressBar.setTextVisible(True)
-        #self.checkBox_query.stateChanged.connect( self.geometry_unitastratigrafiche)### rallenta molto
+        self.comboBox_sito.currentIndexChanged.connect(self.geometry_unitastratigrafiche)### rallenta molto
         
         sito = self.comboBox_sito.currentText()
         self.comboBox_sito.setEditText(sito)
