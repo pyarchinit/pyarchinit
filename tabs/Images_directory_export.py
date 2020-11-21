a35f5ef578c2e0ec64b35cf12302583ac3d5753b
enzococca <enzo.ccc@gmail.com>
2020-11-21 18:18:02 +0100

    add struttura view in media manger

--- tabs/Images_directory_export.py
+++ tabs/Images_directory_export.py
@@ -106,7 +106,7 @@ class pyarchinit_Images_directory_export(QDialog, MAIN_DIALOG_CLASS):
         conn_str = conn.conn_str()
         thumb_resize = conn.thumb_resize()
         thumb_resize_str = thumb_resize['thumb_resize']
-        if self.comboBox_export.currentText()=='Tutte le immagini':
+        if self.comboBox_export.currentText()=='Tutte le immagini \ in periodi e fasi':
             
             us_res = self.db_search_DB('US', 'sito', sito)
             sito_path = '{}{}{}'.format(self.HOME, os.sep, "pyarchinit_image_export")
@@ -343,7 +343,72 @@ class pyarchinit_Images_directory_export(QDialog, MAIN_DIALOG_CLASS):
                 QMessageBox.warning(self, "Alert", "Creazione directories terminata", QMessageBox.Ok)
                 
         
-        
+        if self.comboBox_export.currentText()=='US':
+            
+            us_res = self.db_search_DB('US', 'sito', sito)
+            sito_path = '{}{}{}'.format(self.HOME, os.sep, "pyarchinit_image_export")
+            self.OS_UTILITY.create_dir(sito_path)
+            if bool(us_res):
+                sito_folder =  '{}{}{}'.format(sito_path, os.sep, self.comboBox_sito.currentText() +' - '+ str('Tutte le immagini'))
+                # Periodo_path = '{}{}{}'.format(sito_folder, os.sep, "Periodo")
+                # self.OS_UTILITY.create_dir(Periodo_path)
+                for sing_us in us_res:
+                    sing_per_num = str(sing_us.periodo_iniziale)
+                    prefix = ''
+                    sing_per_num_len = len(sing_per_num)
+                    if sing_per_num_len == 1:
+                        prefix = prefix * 1
+                    
+                    else:
+                        pass
+                    sing_per_dir = prefix + str(sing_per_num)
+                    sing_Periodo_path = ('%s%sPeriodo - %s') % (sito_folder, os.sep, sing_per_dir)
+                    self.OS_UTILITY.create_dir(sing_Periodo_path)
+                    
+                    
+                    
+                    sing_fase_num = str(sing_us.fase_iniziale)
+                    prefix = ''
+                    sing_fase_num_len = len(sing_fase_num)
+                    if sing_fase_num_len == 1:
+                        prefix = prefix * 1
+                    
+                    else:
+                        pass
+                    sing_fase_dir = prefix + str(sing_fase_num)
+                    sing_Fase_path = ('%s%sFase - %s') % (sing_Periodo_path, os.sep, sing_fase_dir)
+                    self.OS_UTILITY.create_dir(sing_Fase_path)
+                        
+                        
+                        
+                    sing_us_num = str(sing_us.us)
+                    prefix = '0'
+                    sing_us_num_len = len(sing_us_num)
+                    if sing_us_num_len == 1:
+                        prefix = prefix * 4
+                    elif sing_us_num_len == 2:
+                        prefix = prefix * 3
+                    elif sing_us_num_len == 3:
+                        prefix = prefix * 2
+                    else:
+                        pass
+                    
+                    
+                    
+                    sing_us_dir = prefix + str(sing_us_num)
+                    sing_US_path = ('%s%sUS - %s') % (sing_Fase_path , os.sep, sing_us_dir)
+                    self.OS_UTILITY.create_dir(sing_US_path)
+
+                    search_dict = {'id_entity': sing_us.id_us, 'entity_type': "'" + "US" + "'"}
+
+                    u = Utility()
+                    search_dict = u.remove_empty_items_fr_dict(search_dict)
+                    search_images_res = self.DB_MANAGER.query_bool(search_dict, 'MEDIAVIEW')
+
+                    for sing_media in search_images_res:
+                        self.OS_UTILITY.copy_file_img(thumb_resize_str+str(sing_media.path_resize), sing_US_path)
+                
+                    search_images_res = ""
         
         # if self.checkBox_US.isChecked()== True:
             # us_res = self.db_search_DB('US', 'sito', sito)
