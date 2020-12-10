3748cd8d24e18356588e2b1de715b58ab08a32fb
enzococca <enzo.ccc@gmail.com>
2020-12-10 16:28:16 +0100

    update

--- resources/dbfiles/pyarchinit_update_postgres.sql
+++ resources/dbfiles/pyarchinit_update_postgres.sql
@@ -688,8 +688,8 @@ INSERT INTO individui_table (
 
                   FROM tafonomia_table
 				  
-				  ON CONFLICT (id_scheda_ind,nr_individuo) DO UPDATE
-				  set sito=EXCLUDED.sito, nr_individuo=EXCLUDED.nr_individuo;
+				  /* ON CONFLICT (id_scheda_ind,nr_individuo) DO UPDATE
+				  set sito=EXCLUDED.sito, nr_individuo=EXCLUDED.nr_individuo; */
 				  
 CREATE OR REPLACE VIEW public.pyarchinit_individui_view AS
  SELECT pyarchinit_individui.gid,
