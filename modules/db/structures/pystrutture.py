94985b9dd7f6fc825d0e70711b870eb6d1a63cb9
enzo <enzo.ccc@gmail.com>
2021-06-19 13:13:09 +0200

    Update pystrutture.py

--- modules/db/structures/pystrutture.py
+++ modules/db/structures/pystrutture.py
@@ -33,7 +33,7 @@ class pystrutture:
                      Column('fase_iniz', Integer),
                      Column('fase_fin', Integer),
                      Column('descrizione', Text),
-                     Column('the_geom', Geometry('POLYGON',-1)),
+                     Column('the_geom', Text),
                      Column('sigla_strut', Text),
                      Column('nr_strut', Integer),
                      # explicit/composite unique constraint.  'name' is optional.
