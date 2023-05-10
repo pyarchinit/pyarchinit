42a58b374f69b771feadbafc34cd146bc3194851
enzo <enzo.ccc@gmail.com>
2023-05-10 11:08:04 +0200

    add char gpt into thesaurus

--- modules/utility/askgpt.py
+++ modules/utility/askgpt.py
@@ -1,11 +1,6 @@
 
-import os
-import subprocess
-import sys
-import time
-
 from qgis.PyQt.QtWidgets import *
-
+import sys, subprocess
 try:
     import openai
 
@@ -21,55 +16,25 @@ except ImportError:
     else:
         raise Exception(f"Unsupported platform: {sys.platform}")
     print("openai installed successfully")
+import time
+import os
 class MyApp(QWidget):
 
-    def __init__(self, api_key, parent=None):
-        super().__init__(parent)
-        self.api_key = api_key
+	def __init__(self,parent):
+		super().__init__(parent)
 
-    def ask_gpt(self, prompt):
-
-        openai.api_key = self.api_key
-        for _ in range(3):  # retry up to 3 times
-            try:
-                response = openai.Completion.create(
-                    engine="davinci",
-                    prompt=prompt,
-                    temperature=0.8,
-                    max_tokens=2048
-                )
-                return response.choices[0].text.strip()
-            except openai.error.APIError as e:
-                if e.status == 429:
-                    time.sleep(5)
-                else:
-                    raise e
-        raise Exception("Failed to call OpenAI API after 3 retries")
-
-def install_openai():
-    try:
-        import openai
-        print("openai is already installed")
-    except ImportError:
-        print("openai is not installed, installing...")
-        if sys.platform.startswith("win"):
-            subprocess.check_call(["pip", "install", "openai"])
-        elif sys.platform.startswith("darwin"):
-            subprocess.check_call(["pip3", "install", "openai"])
-        elif sys.platform.startswith("linux"):
-            subprocess.check_call(["pip", "install", "openai"])
-        else:
-            raise Exception(f"Unsupported platform: {sys.platform}")
-        print("openai installed successfully")
-
-if __name__ == "__main__":
-    install_openai()
-    app = QApplication(sys.argv)
-    api_key = os.environ.get("OPENAI_API_KEY")
-    if not api_key:
-        QMessageBox.critical(None, "Error", "OPENAI_API_KEY environment variable not set")
-        sys.exit(1)
-    window = MyApp(api_key)
-    window.show()
-    sys.exit(app.exec_())
-
+	def ask_gpt(self,prompt,apikey):
+		openai.api_key = apikey
+		while True:
+			try:
+				response = openai.ChatCompletion.create(
+					model = "gpt-3.5-turbo",
+					messages = [
+						{"role": "system", "content": prompt}]
+				)
+				return response['choices'][0]['message']['content']
+			except openai.error.APIError as e:
+				if e.status == 429:
+					time.sleep(5)
+				else:
+					raise e
