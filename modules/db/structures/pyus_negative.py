b2bee2c05c10c0cbd967da8744395d62089a18bd
enzo <enzo.ccc@gmail.com>
2023-08-11 04:59:05 +0200

    -fix bug dta import into postigis

--- modules/db/structures/pyus_negative.py
+++ modules/db/structures/pyus_negative.py
@@ -30,7 +30,7 @@ class pyus_negative:
                      Column('us_n', Integer),
                      Column('tipo_doc_n', Text),
                      Column('nome_doc_n', Text),
-                     Column('the_geom', Geometry(geometry_type='LINESTARING')),
+                     Column('the_geom', Geometry(geometry_type='LINESTRING')),
                      # explicit/composite unique constraint.  'name' is optional.
                      UniqueConstraint('gid')
                      )
