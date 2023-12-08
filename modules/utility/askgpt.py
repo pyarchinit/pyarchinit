03d64efd62a3d4efd60388c59ea13d914549a807
enzo <enzo.ccc@gmail.com>
2023-12-08 15:38:02 +0400

    Refactor code to improve readability and handle different GPT models in the `ask_gpt` method, and add a method to check internet connectivity.

--- modules/utility/askgpt.py
+++ modules/utility/askgpt.py
@@ -1,5 +1,6 @@
 
 from qgis.PyQt.QtWidgets import *
+import socket
 import sys, subprocess
 try:
     import openai
@@ -10,7 +11,7 @@ except ImportError:
     if sys.platform.startswith("win"):
         subprocess.check_call(["pip", "install", "openai"],shell = False)
     elif sys.platform.startswith("darwin"):
-        subprocess.check_call(["pip3", "install", "openai"],shell = False )
+        subprocess.check_call([ "python3", "-m", "pip", "install", "openai"],shell = False )
     elif sys.platform.startswith("linux"):
         subprocess.check_call(["pip", "install", "openai"],shell = False)
     else:
@@ -20,21 +21,31 @@ import time
 import os
 class MyApp(QWidget):
 
-	def __init__(self,parent):
-		super().__init__(parent)
+    def __init__(self,parent):
+        super().__init__(parent)
 
-	def ask_gpt(self,prompt,apikey):
-		openai.api_key = apikey
-		while True:
-			try:
-				response = openai.ChatCompletion.create(
-					model = "gpt-3.5-turbo-16k",
-					messages = [
-						{"role": "system", "content": prompt}]
-				)
-				return response['choices'][0]['message']['content']
-			except openai.error.APIError as e:
-				if e.status == 429:
-					time.sleep(5)
-				else:
-					raise e
+    def ask_gpt(self,prompt,apikey,model):
+        openai.api_key = apikey
+        while True:
+            try:
+                response = openai.ChatCompletion.create(
+                    model = model,
+                    messages = [
+                        {"role": "system", "content": prompt}]
+                )
+                return response['choices'][0]['message']['content']
+            except openai.error.APIError as e:
+                if e.status == 429:
+                    time.sleep(5)
+                else:
+                    raise e
+
+    def is_connected(self):
+        try:
+            # Try to connect to one of the DNS servers
+            socket.create_connection(("1.1.1.1", 53))
+            return True
+        except OSError:
+            pass
+        return False
+
