ed94e43b0b685eebe1ffb8f7bba0180095988e0d
enzo <enzo.ccc@gmail.com>
2024-03-17 08:09:51 +0100

    Uncomment database structure metadata and definitions
    The code change uncommented several lines in database_schema.py, which import modules from the 'structures_metadata' subdirectory and call their 'define_table' methods. These changes enable additional database tables to be defined and utilized.

--- modules/utility/database_schema.py
+++ modules/utility/database_schema.py
@@ -16,7 +16,7 @@ from modules.db.structures_metadata.Media_to_Entity_table_view import Media_to_E
 from modules.db.structures_metadata.Periodizzazione_table import Periodizzazione_table
 # from modules.db.structures_metadata.Pyarchinit_thesaurus_sigle import Pyarchinit_thesaurus_sigle
 from modules.db.structures_metadata.SCHEDAIND_table import SCHEDAIND_table
-# from modules.db.structures_metadata.Site_table import Site_table
+from modules.db.structures_metadata.Site_table import Site_table
 from modules.db.structures_metadata.Struttura_table import Struttura_table
 from modules.db.structures_metadata.Tomba_table import Tomba_table
 from modules.db.structures_metadata.US_table import US_table
@@ -24,21 +24,21 @@ from modules.db.structures_metadata.US_table import US_table
 # from modules.db.structures_metadata.UT_table import UT_table
 from modules.db.structures_metadata.pyunitastratigrafiche import pyunitastratigrafiche
 from modules.db.structures_metadata.pyunitastratigrafiche_usm import pyunitastratigrafiche_usm
-# from modules.db.structures_metadata.pysito_point import pysito_point
-# from modules.db.structures_metadata.pysito_polygon import pysito_polygon
-# from modules.db.structures_metadata.pyquote import pyquote
-# from modules.db.structures_metadata.pyquote_usm import pyquote_usm
-# from modules.db.structures_metadata.pyus_negative import pyus_negative
-# from modules.db.structures_metadata.pystrutture import pystrutture
+from modules.db.structures_metadata.pysito_point import pysito_point
+from modules.db.structures_metadata.pysito_polygon import pysito_polygon
+from modules.db.structures_metadata.pyquote import pyquote
+from modules.db.structures_metadata.pyquote_usm import pyquote_usm
+from modules.db.structures_metadata.pyus_negative import pyus_negative
+from modules.db.structures_metadata.pystrutture import pystrutture
 from modules.db.structures_metadata.pyreperti import pyreperti
-# from modules.db.structures_metadata.pyindividui import pyindividui
-# from modules.db.structures_metadata.pycampioni import pycampioni
-# from modules.db.structures_metadata.pytomba import pytomba
-# from modules.db.structures_metadata.pydocumentazione import pydocumentazione
-# from modules.db.structures_metadata.pylineeriferimento import pylineeriferimento
-# from modules.db.structures_metadata.pyripartizioni_spaziali import pyripartizioni_spaziali
-# from modules.db.structures_metadata.pysezioni import pysezioni
-#from modules.db.structures_metadata.Pottery_table import *
+from modules.db.structures_metadata.pyindividui import pyindividui
+from modules.db.structures_metadata.pycampioni import pycampioni
+from modules.db.structures_metadata.pytomba import pytomba
+from modules.db.structures_metadata.pydocumentazione import pydocumentazione
+from modules.db.structures_metadata.pylineeriferimento import pylineeriferimento
+from modules.db.structures_metadata.pyripartizioni_spaziali import pyripartizioni_spaziali
+from modules.db.structures_metadata.pysezioni import pysezioni
+
 from ..db.structures_metadata.Pottery_table import PotteryTable
 
 # Crea una singola connessione e MetaData per l'applicazione
@@ -60,7 +60,7 @@ Media_to_Entity_table_view.define_table(metadata)
 Periodizzazione_table.define_table(metadata)
 # Pyarchinit_thesaurus_sigle(metadata)
 SCHEDAIND_table.define_table(metadata)
-# Site_table(metadata)
+Site_table.define_table(metadata)
 Struttura_table.define_table(metadata)
 Tomba_table.define_table(metadata)
 US_table.define_table(metadata)
@@ -68,20 +68,20 @@ US_table.define_table(metadata)
 # UT_table(metadata)
 pyunitastratigrafiche.define_table(metadata)
 pyunitastratigrafiche_usm.define_table(metadata)
-# pysito_point(metadata)
-# pysito_polygon(metadata)
-# pyquote(metadata)
-# pyquote_usm(metadata)
-# pyus_negative(metadata)
-# pystrutture(metadata)
+pysito_point.define_table(metadata)
+pysito_polygon.define_table(metadata)
+pyquote.define_table(metadata)
+pyquote_usm.define_table(metadata)
+pyus_negative.define_table(metadata)
+pystrutture.define_table(metadata)
 pyreperti.define_table(metadata)
-# pyindividui(metadata)
-# pycampioni(metadata)
-# pytomba(metadata)
-# pydocumentazione(metadata)
-# pylineeriferimento(metadata)
-# pyripartizioni_spaziali(metadata)
-# pysezioni(metadata)
+pyindividui.define_table(metadata)
+pycampioni.define_table(metadata)
+pytomba.define_table(metadata)
+pydocumentazione.define_table(metadata)
+pylineeriferimento.define_table(metadata)
+pyripartizioni_spaziali.define_table(metadata)
+pysezioni.define_table(metadata)
 PotteryTable.define_table(metadata)
 
 # Creare tutte le tabelle nel database
