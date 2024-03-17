3f84353f94b3a8c39c94a1dbbe1b80d3185e8cdd
enzo <enzo.ccc@gmail.com>
2024-03-17 08:09:01 +0100

    Refactor pytomba class in db module
    This commit simplifies and improves pytomba class by removing unnecessary variables and methods. It introduces a new, more reusable and efficient class method called 'define_table' for table definition. Additionally, metadata creation at initialization has been removed for efficiency and better control over the process.

--- modules/db/structures_metadata/pytomba.py
+++ modules/db/structures_metadata/pytomba.py
@@ -10,20 +10,10 @@ from modules.db.pyarchinit_conn_strings import Connection
 from modules.utility.pyarchinit_OS_utility import Pyarchinit_OS_Utility
 
 class pytomba:
-    
-    
-    # connection string postgres"
-    internal_connection = Connection()
 
-    # create engine and metadata
-
-    engine = create_engine(internal_connection.conn_str(), echo=False, convert_unicode=True)
-    
-    #engine.connect()
-    metadata = MetaData(engine)
-
-    # define tables check per verifica fill fields 20/10/2016 OK
-    pytomba = Table('pyarchinit_tafonomia', metadata,
+    @classmethod
+    def define_table(cls, metadata):
+        return Table('pyarchinit_tafonomia', metadata,
                      Column('gid', Integer, primary_key=True),  # 0
                      Column('sito', Text),
                      Column('nr_scheda', Integer),
@@ -31,6 +21,3 @@ class pytomba:
                      # explicit/composite unique constraint.  'name' is optional.
                      UniqueConstraint('gid')
                      )
-
-    metadata.create_all(engine)
-    
